0a5688a19ff8b218d47729c64abef163ac8458a1
Christopher Chew <fishkepr@comcast.net>
2018-01-27 17:50:23 -0800

    Cleaned Up Code

--- js/sales.js
+++ js/sales.js
@@ -2,7 +2,7 @@
 
 var hours = ['6am', '7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
-function Location (name, minCust, maxCust,avgSale) {
+function LocationObject (name, minCust, maxCust,avgSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -15,7 +15,6 @@ function Location (name, minCust, maxCust,avgSale) {
       var custPerHour = getRandomCustomerNum(this.minCust,this.maxCust);
       this.hourlySales.push(Math.floor(custPerHour*this.avgSale));
       this.salesForDay+=this.hourlySales[i];
-      console.log('Cust per hour: ' + custPerHour);
     }
   };
   this.render = function(){
@@ -27,13 +26,16 @@ function Location (name, minCust, maxCust,avgSale) {
 
 
 //Common Functions
+
+//Loads Detail line to table
 function appendLine(name, hourlySales,salesForDay){
   //Load Location Variables to HTML
     
   //Load Location Name
   var salesTable = document.getElementById('salesTable');
   
-  //Load Location Detail Line
+  //Append Location Detail Line to table
+  //Need both Table Row AND Table Header for each detail line
   var trEl = document.createElement('tr');
   salesTable.appendChild(trEl);
 
@@ -41,25 +43,24 @@ function appendLine(name, hourlySales,salesForDay){
   thEl.textContent = name;
   salesTable.appendChild(thEl);
   
-  //var locData = document.getElementById('locData');    
+  //Loop Through and append Hours per Location
   for (var i=0; i<hours.length; i++){
     var tdEl = document.createElement('td');
     tdEl.textContent = hourlySales[i];
-    console.log('Detail Loop: '+i);
-    console.log('Hourly Sales: '+tdEl.textContent);
     salesTable.appendChild(tdEl);
   }
-  // Load Total Sales for Location
+  //Append Total Sales for Location
   tdEl = document.createElement('td');
   tdEl.textContent = salesForDay;
   salesTable.appendChild(tdEl);
-    
 }
 
+//Generates Random Number of Customers
 function getRandomCustomerNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-//********   Mainline ********
+
+//********   Mainline Starts Here   ********
 
 //Create Table Header
 var salesTable = document.getElementById('salesTable');
@@ -69,7 +70,7 @@ salesTable.appendChild(thEl);
 for (var i=0; i<hours.length; i++) {
   thEl = document.createElement('th');
   thEl.textContent = hours[i];
-  console.log('Column Header: '+thEl.textContent);
+
   salesTable.appendChild(thEl);
 }
 
@@ -79,11 +80,11 @@ salesTable.appendChild(thEl);
 
 //Create locations and Populate Location Data
 
-var fandp = new Location("First and Pike", 23, 65, 6.3);
-var seatac = new Location("SeaTac Airport", 3, 24, 1.2);
-var seattleCenter = new Location("Seattle Center", 11, 38, 3.7);
-var capitolHill = new Location("Capitol Hill", 20, 38, 2.3);
-var alki = new Location("Alki", 2, 16, 4.6);
+var fandp = new LocationObject("First and Pike", 23, 65, 6.3);
+var seatac = new LocationObject("SeaTac Airport", 3, 24, 1.2);
+var seattleCenter = new LocationObject("Seattle Center", 11, 38, 3.7);
+var capitolHill = new LocationObject("Capitol Hill", 20, 38, 2.3);
+var alki = new LocationObject("Alki", 2, 16, 4.6);
 
 
 fandp.loadHourlySales();
